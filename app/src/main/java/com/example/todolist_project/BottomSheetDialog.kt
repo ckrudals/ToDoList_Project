527530ab727006fbd521df5594584ddf1faacfeb
KyungMin <2004ckck@naver.com>
2021-02-24 23:40:55 +0900

    [Update] xml 변경

--- app/src/main/java/com/example/todolist_project/BottomSheetDialog.kt
+++ app/src/main/java/com/example/todolist_project/BottomSheetDialog.kt
@@ -1,17 +1,34 @@
 package com.example.todolist_project
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.ContentValues.TAG
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.transition.Visibility
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.lifecycleScope
+import androidx.room.Room
+import com.example.todolist_project.Room.Main
+import com.example.todolist_project.Room.MainDatabase
 import com.example.todolist_project.databinding.BottomsheetdialogBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.*
+
+
+@Suppress("CAST_NEVER_SUCCEEDS", "ASSIGNED_BUT_NEVER_ACCESSED_VARIABLE")
 
 class BottomSheetDialog : BottomSheetDialogFragment() {
+
+
     private var _binding: BottomsheetdialogBinding? = null
     private val binding get() = _binding!!
     private lateinit var bottomSheetDialogClickListener: BottomSheetDialogClickListener
@@ -21,6 +38,8 @@ class BottomSheetDialog : BottomSheetDialogFragment() {
 
     }
 
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -30,7 +49,13 @@ class BottomSheetDialog : BottomSheetDialogFragment() {
 
         _binding = BottomsheetdialogBinding.inflate(inflater, container, false)
         val view = binding.root
+
+
+
+
+
         return view
+
 
     }
 
@@ -38,19 +63,99 @@ class BottomSheetDialog : BottomSheetDialogFragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        binding.timeGoal.setOnClickListener(){
 
+        val db = Room.databaseBuilder(context!!, MainDatabase::class.java, "Main")
+            .fallbackToDestructiveMigration()
+            .build()
 
+        timedialog()
+
+        daydialog()
+
+        binding.timeSave.setOnClickListener {
+            val intent = Intent(context, MainActivity::class.java)
+            lifecycleScope.launch(Dispatchers.IO) {
+
+                val day = intent.getStringExtra("day_room")
+                val time = intent.getStringExtra("time_room")
+                db.mainDao().insert(
+                    Main(
+                        db_goal = binding.timeGoal.text.toString(),
+                        db_time = time,
+                        db_day = day
+                    )
+                )
+
+                intent.putExtra("goal", binding.timeGoal.text)
+                Log.d(TAG, "goal: ${binding.timeGoal.text} ")
+                intent.putExtra("time",day)
+                Log.d(TAG, "time: $day ")
+                intent.putExtra("day", time)
+                Log.d(TAG, "day: $time")
+                save()
+            }
+            startActivity(intent)
 
         }
-        binding.timeSave.setOnClickListener() {
 
-            save()
-            binding.timeButonText.text = binding.timeButonText.toString()
-binding.timeGoal
-            //데이터 저장
+
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun daydialog() {
+        binding.dayButonText.setOnClickListener {
+
+            var intent = Intent()
+
+            var calendar = Calendar.getInstance()
+            var year = calendar.get(Calendar.YEAR)
+            var month = calendar.get(Calendar.MONTH)
+            var day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            var listener = DatePickerDialog.OnDateSetListener { datePicker, i, i2, i3 ->
+                // i년 i2월 i3일
+                var day_text = binding.dayButonText.text
+                binding.dayButonText.text = " ${i2 + 1}월 ${i3}일"
+                intent.putExtra("day_room", day_text)
+                Log.d(TAG,  " ${i2 + 1}월 ${i3}일")
+            }
+
+            var picker = DatePickerDialog(context!!, listener, year, month, day)
+            picker.show()
 
         }
+
+
+    }
+
+
+
+
+
+    fun timedialog() {
+
+
+        binding.timeButonText.setOnClickListener {
+            var intent = Intent()
+
+            var calendar = Calendar.getInstance()
+            var hour = calendar.get(Calendar.HOUR)
+            var minute = calendar.get(Calendar.MINUTE)
+
+            var listener = TimePickerDialog.OnTimeSetListener { timePicker, i, i2 ->
+                var time_text =  binding.timeButonText.text
+                binding.timeButonText.text = "${i}시 ${i2}분"
+                intent.putExtra("time_room", time_text)
+                Log.d(TAG, "${i}시 ${i2}분")
+            }
+
+            var picker = TimePickerDialog(context!!, listener, hour, minute, false) // true하면 24시간 제
+            picker.show()
+
+        }
+
+
     }
 
     override fun onDestroyView() {
@@ -74,7 +179,12 @@ binding.timeGoal
         binding.timeGoal.isEnabled = isHidden
         binding.timeButonText.isEnabled = isHidden
 
+        binding.timeButonText.text = binding.timeButonText.toString()
+        binding.dayButonText.text = binding.dayButonText.toString()
+        binding.timeGoal.text = binding.timeGoal.text
 
     }
 }
+
+
 
